cf4692a0f948e947d0f6b2dd88022bf7823d6275
Trent <trent.polenchar.2021@student.lhschools.org>
2019-05-10 14:03:31 -0400

    Game works

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -8,63 +8,59 @@ public class RockPaperScissors {
         keyboard = new Scanner(System.in);
 
 
-        String computerAnswer;
+        int computerAnswer;
         String userAnswer;
-
-
-
 
 
 
         int[] game = {1, 2, 3};
 
 
+        computerAnswer = (game[(int)(Math.random()*2)]);
 
-        for(int rockpaperscissors: game){
-            rockpaperscissors = (int) (Math.random() * 3);
-            if (rockpaperscissors == 0){
-                computerAnswer = "Rock";
 
-            } else if (rockpaperscissors == 1){
-                computerAnswer = "Paper";
 
-            } else if (rockpaperscissors == 2){
-                computerAnswer = "Scissors";
-            }
-        }
+        System.out.println("This is a test " + computerAnswer);
+
+
 
         System.out.println("Enter in Rock Paper or Scissors");
 
         userAnswer = keyboard.nextLine().toLowerCase();
 
-        computerAnswer = "none";
 
-        if((userAnswer.equals ("rock"))&(computerAnswer == "Rock")){
-            System.out.println("You both picked rock");
+        //0 is rock
+        //1 is paper
+        //2 is scissors
+
+
+
+        if((userAnswer.equals ("rock"))& computerAnswer == 0){
+            System.out.println("You both picked rock. It's a tie");
         }
-        else if((userAnswer.equals ("paper"))&(computerAnswer == "Paper")){
-            System.out.println("You both picked paper");
+        else if((userAnswer.equals ("paper"))& computerAnswer == 1){
+            System.out.println("You both picked paper. It's a tie");
         }
-        else if((userAnswer.equals ("scissors"))&(computerAnswer == "Scissors")) {
-            System.out.println("You both picked scissors");
+        else if((userAnswer.equals ("scissors"))& computerAnswer == 2) {
+            System.out.println("You both picked scissors. It's a tie");
         }
-        else if((userAnswer.equals ("scissors"))&(computerAnswer == "Rock")) {
+        else if((userAnswer.equals ("scissors"))& computerAnswer == 0) {
             System.out.println("You lost to rock");
         }
-        else if((userAnswer.equals ("rock"))&(computerAnswer == "Paper")) {
+        else if((userAnswer.equals ("rock"))& computerAnswer == 1) {
             System.out.println("You lost to paper");
         }
-        else if((userAnswer.equals ("paper"))&(computerAnswer == "Scissors")) {
+        else if((userAnswer.equals ("paper"))& computerAnswer == 2) {
             System.out.println("You lost to scissors");
         }
-        else if((userAnswer.equals ("paper"))&(computerAnswer == "Rock")) {
-            System.out.println("You won against rock");
+        else if((userAnswer.equals ("paper"))& computerAnswer == 0) {
+            System.out.println("You won against rock!");
         }
-        else if((userAnswer.equals ("scissors"))&(computerAnswer == "Paper")) {
-            System.out.println("You won against paper");
+        else if((userAnswer.equals ("scissors"))& computerAnswer == 1) {
+            System.out.println("You won against paper!");
         }
-        else if((userAnswer.equals ("rock"))&(computerAnswer == "Scissors")) {
-            System.out.println("You won against scissors");
+        else if((userAnswer.equals ("rock"))&  computerAnswer == 2) {
+            System.out.println("You won against scissors!");
         }
         else{
             System.out.println("That is not a valid answer");
@@ -74,6 +70,8 @@ public class RockPaperScissors {
         boolean loop = false;
         while (loop) {
             System.out.println("");
+
+
 
 
 
