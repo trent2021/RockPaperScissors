7dddd48f04b42f39fb5c353214ce6f9cc7d83324
Trent <trent.polenchar.2021@student.lhschools.org>
2019-05-13 13:43:36 -0400

    Added score counter

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -9,6 +9,10 @@ public class RockPaperScissors {
 
         int computerAnswer;
         String userAnswer;
+        int computerPoints;
+        computerPoints = 0;
+        int userPoints;
+        userPoints = 0;
         int[] game = {1, 2, 3};
 
         String stop;
@@ -45,26 +49,33 @@ public class RockPaperScissors {
             }
             else if((userAnswer.equals ("scissors"))& computerAnswer == 0) {
                 System.out.println("You lost to rock");
+                computerPoints++;
             }
             else if((userAnswer.equals ("rock"))& computerAnswer == 1) {
                 System.out.println("You lost to paper");
+                computerPoints++;
             }
             else if((userAnswer.equals ("paper"))& computerAnswer == 2) {
                 System.out.println("You lost to scissors");
+                computerPoints++;
             }
             else if((userAnswer.equals ("paper"))& computerAnswer == 0) {
                 System.out.println("You won against rock!");
+                userPoints++;
             }
             else if((userAnswer.equals ("scissors"))& computerAnswer == 1) {
                 System.out.println("You won against paper!");
+                userPoints++;
             }
             else if((userAnswer.equals ("rock"))&  computerAnswer == 2) {
                 System.out.println("You won against scissors!");
+                userPoints++;
             }
             else{
                 System.out.println("That is not a valid answer");
             }
 
+            System.out.println("Your points = " + userPoints + "   " + "Computer's points = " + computerPoints);
 
             System.out.println("Would you like to continue? Y/N");
 
